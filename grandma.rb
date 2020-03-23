c13496dfbd3f274a821069aa2c49f4377c15214d
Justin Winger <justinwinger4@gmail.com>
2020-03-23 18:59:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
-  elseif input != input.upcase
+  elseif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  else input == input.upcase
+  else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
 end
 
